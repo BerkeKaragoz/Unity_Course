2124bd8551674a06c1ffb6d10cff179d4957eb9e
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2017-12-17 23:08:49 +0300

    Terminal Hacker 3
    The first Function. (showMainmenu)
    ClearScreen.

--- 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         print("Buradayim konsol!");
+        showMainmenu();
+    }
+
+    void showMainmenu()
+    {
+        Terminal.ClearScreen();
         Terminal.WriteLine("ITerminal Since 1968.\n\nPress 1 for your own terminal.\nPress 2 for NASA.\nPress 3 for Black Rainbow.");
-	}
-	
+    }
+
+
 	// Update is called once per frame
 	void Update () {
 		

cac60d733f6cf2ed585fb0c0af6c20355775cd31
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2017-12-17 22:52:17 +0300

    Terminal Hacker 2
    Printing to the console and terminal. Terminal.Writeline("...");

---
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Buradayim konsol!");
+        Terminal.WriteLine("ITerminal Since 1968.\n\nPress 1 for your own terminal.\nPress 2 for NASA.\nPress 3 for Black Rainbow.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

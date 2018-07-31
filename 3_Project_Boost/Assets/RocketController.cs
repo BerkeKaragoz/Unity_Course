c35f5787d948169a179987ccf7c1d467532479e0
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2018-07-31 22:36:16 +0300

    Controls + Particle
    + Debug function

---
+++ 3_Project_Boost/Assets/RocketController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+    [SerializeField]
+    float PushingPower = 100f;
+    [SerializeField]
+    float RotationMultiplier = 3f; 
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+     //   DebugLog();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(0, PushingPower, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(0, 0, -PushingPower*RotationMultiplier*Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(0, 0, PushingPower*RotationMultiplier*Time.deltaTime);
+        }
+    }
+    private void DebugLog()
+    {
+        print(Time.deltaTime);
+    }
+}

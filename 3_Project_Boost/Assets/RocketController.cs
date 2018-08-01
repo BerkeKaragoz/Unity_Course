a3bd16c949a010cb43cd633f297cf710eabdd6e3
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2018-08-01 17:56:03 +0300

    Thruster Particle + New Rocket + Delta Time

--- 3_Project_Boost/Assets/RocketController.cs
+++ 3_Project_Boost/Assets/RocketController.cs
@@ -3,40 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RocketController : MonoBehaviour {
+public class RocketController : MonoBehaviour
+{
 
     [SerializeField]
     float PushingPower = 100f;
     [SerializeField]
-    float RotationMultiplier = 3f; 
+    float RotationMultiplier = 3f;
+
     Rigidbody rb;
+    AudioSource audiosource;
+    ParticleSystem ps;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        ProcessInput();
-     //   DebugLog();
-	}
-
-    private void ProcessInput()
+    // Use this for initialization
+    void Start()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddRelativeForce(0, PushingPower, 0);
-        }
+        rb = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+        //   DebugLog();
+    }
+
+    private void Rotate()
+    {
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddTorque(0, 0, -PushingPower*RotationMultiplier*Time.deltaTime);
+            rb.AddTorque(0, 0, -PushingPower * RotationMultiplier * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            rb.AddTorque(0, 0, PushingPower*RotationMultiplier*Time.deltaTime);
+            rb.AddTorque(0, 0, PushingPower * RotationMultiplier * Time.deltaTime);
         }
     }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(0, PushingPower*Time.deltaTime, 0);
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+                ps.Play();
+            }
+        }
+        else { audiosource.Stop(); ps.Stop(); }
+    }
+
     private void DebugLog()
     {
         print(Time.deltaTime);

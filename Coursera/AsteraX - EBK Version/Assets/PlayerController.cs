2d73b4103149801db89260e8b02dcb96a6047a26
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2018-10-27 01:13:15 +0300

    Projectile Instantiation Process

--- Coursera/AsteraX - EBK Version/Assets/PlayerController.cs
+++ Coursera/AsteraX - EBK Version/Assets/PlayerController.cs
@@ -2,57 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody ProjectilePrefab;
+    public Transform ExitLocation;
 
     [SerializeField]
     private float SpeedMultiplier = 2500f;
-    
 
-    private Rigidbody rb;
+
+    private Rigidbody player;
     private Camera mainCamera;
     private RaycastHit hit;
+    private Vector3 aimPoint = new Vector3(0, 0, 0);
+    private ProjectileShooter Shooter = new ProjectileShooter(ProjectilePrefab, ExitLocation);
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
         mainCamera = FindObjectOfType<Camera>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-        LookAtMouse();
+    // Update is called once per frame
+    void Update()
+    {
+
+        getAimPoint();
+        transform.LookAt(aimPoint, Vector3.up);//Looks at the mouse
         Move();
-        
+
+        if (Input.GetButton("Fire1"))
+            Shooter.Shoot();
     }
 
     public void Move()
     {
-            rb.AddForce(0, 0, Input.GetAxisRaw("Vertical") * SpeedMultiplier * Time.deltaTime);
-            rb.AddForce(Input.GetAxisRaw("Horizontal") * SpeedMultiplier * Time.deltaTime, 0, 0);
+        player.AddForce(0, 0, Input.GetAxisRaw("Vertical") * SpeedMultiplier * Time.deltaTime);
+        player.AddForce(Input.GetAxisRaw("Horizontal") * SpeedMultiplier * Time.deltaTime, 0, 0);
 
         //this.transform.SetPositionAndRotation(this.transform.position, Quaternion.Euler(this.transform.rotation.x + 20f * Input.GetAxis("Vertical"), this.transform.rotation.y, this.transform.rotation.z + 20f * -Input.GetAxis("Horizontal")));
 
-        this.transform.SetPositionAndRotation(this.transform.position, new Quaternion(Mathf.PI * Input.GetAxis("Vertical") / 12, this.transform.rotation.y,  Mathf.PI * -Input.GetAxis("Horizontal") / 12, this.transform.rotation.w));
+        this.transform.SetPositionAndRotation(this.transform.position, new Quaternion(Mathf.PI * Input.GetAxis("Vertical") / 12, this.transform.rotation.y, Mathf.PI * -Input.GetAxis("Horizontal") / 12, this.transform.rotation.w));
 
         print(this.transform.rotation);
 
     }
 
-    public void LookAtMouse()
+    public Vector3 getAimPoint()
     {
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
         float rayLenght;
 
         if (groundPlane.Raycast(cameraRay, out rayLenght))
         {
-            Vector3 aimPoint = cameraRay.GetPoint(rayLenght);
+            aimPoint = cameraRay.GetPoint(rayLenght);
             Debug.DrawLine(this.transform.position, aimPoint, Color.blue);
-
-            transform.LookAt(aimPoint, Vector3.up);
-            //transform.LookAt()
         }
+        return aimPoint;
     }
+
 }
 
